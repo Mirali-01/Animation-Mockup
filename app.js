5e1ad5f375cb71b81840c46a39ad416216c64940
Mir <mir.m.ali01@gmail.com>
2023-02-10 00:04:20 -0500

    basic functions

---
+++ app.js
@@ -0,0 +1,31 @@
+// find a way to connect surprise button to show an image on-click and to perform key frames
+
+
+const roundRound = () => {
+    let spinRight = document.querySelector(".elementRight")
+    spinRight.classList.toggle("spin")
+
+    let spinLeft = document.querySelector(".elementLeft")
+    spinLeft.classList.toggle("revspin")
+}
+
+const bounce = () => {
+    let bunnies = document.querySelector(".ball")
+    bunnies.classList.toggle("jump")
+}
+
+const right = () => {
+    let toRight = document.querySelector(".rectangle")
+    toRight.classList.toggle("right")
+}
+const diagonal = () => {
+    let toDiagonal = document.querySelector(".square")
+    toDiagonal.classList.toggle("diagonal")
+}
+
+const zoomer = () => {
+    let timeWaster = document.querySelector(".tada")
+    timeWaster.classList.toggle("zoom")
+    
+}
+
